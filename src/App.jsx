d9395d02aa168fac60b97ec3ea72899fdbce264b
kachitaro <anhtai.dev@gmail.com>
2025-10-21 12:24:26 +0700

    windows

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import './App.css'
 import UsbDemo from './UsbDemo.jsx'
 import WebUSBDemo from './WebUSBDemo.jsx'
 function App() {
   return (
     <>
       <h1>Vite + React</h1>
-      <UsbDemo />
+      {/* <UsbDemo /> */}
       <WebUSBDemo />
     </>
   )
